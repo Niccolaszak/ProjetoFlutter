e176541b9c711aee1d3f03ffe3bf4d52a7226714
Nicolas Miguel Uczak <nicolasmiuczak@gmail.com>
2026-08-28 10:19:58 -0300

    Deixando a tela principal interativa
    Adicionado funcionalidades de inserção e listagem de gastos recentes, além de calculo do total dos gastos para exibi-los nos SummaryCards

--- controle_de_gastos/lib/pages/controle_page.dart
+++ controle_de_gastos/lib/pages/controle_page.dart
@@ -3,6 +3,7 @@ import 'package:controle_de_gastos/widgets/expense_tile.dart';
 import 'package:controle_de_gastos/widgets/summary_card.dart';
 import 'package:flutter/material.dart';
 import 'package:controle_de_gastos/models/expense.dart';
+import 'dart:math';
 
 class ControlePage extends StatefulWidget {
   const ControlePage({super.key});
@@ -22,6 +23,61 @@ class _ControlePageState extends State<ControlePage> {
   // Lista que vai armazenar todos os gastos
   List<Expense> expenses = [];
 
+  List<double> parseNumbers(String expression) {
+    RegExp regExp = RegExp(r"[0-9]+\.?[0-9]*");
+
+    var matches = regExp.allMatches(expression);
+    List<double> numbers = [];
+
+    for (var match in matches) {
+      String numberText = match.group(0)!;
+      numbers.add(double.parse(numberText));
+    }
+
+    return numbers;
+  }
+
+  void adicionarGasto() {
+    final String descricao = descriptionController.text;
+    final String valorTexto = amountController.text;
+
+    if (descricao.isEmpty || valorTexto.isEmpty) return;
+
+    String expression = valorTexto.replaceAll(',', '.');
+    List<double> numbers = parseNumbers(expression);
+
+    if (numbers.isEmpty) return;
+
+    final double valor = numbers.first;
+
+    if (valor <= 0) return;
+
+    setState(() {
+      expenses.insert(
+        0,
+        Expense(
+          description: descricao,
+          amount: valor,
+          category: _selectedCategory,
+        ),
+      );
+    });
+
+    descriptionController.clear();
+    amountController.clear();
+  }
+
+  //calcula o valor total dos gastos
+  double calcularTotal() {
+    return expenses.fold(0.0, (soma, item) => soma + item.amount);
+  }
+
+  double totalCategoria(CategoryType categoria) {
+    return expenses
+        .where((item) => item.category == categoria)
+        .fold(0.0, (soma, item) => soma + item.amount);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,7 +95,7 @@ class _ControlePageState extends State<ControlePage> {
           children: [
             SummaryCard(
               title: "Gasto Total",
-              amount: "0,00",
+              amount: calcularTotal().toStringAsFixed(2).replaceAll(".", ","),
               color: Colors.red,
             ),
             Row(
@@ -47,14 +103,18 @@ class _ControlePageState extends State<ControlePage> {
                 Expanded(
                   child: SummaryCard(
                     title: CategoryType.alimentacao.label,
-                    amount: "0,00",
+                    amount: totalCategoria(CategoryType.alimentacao)
+                        .toStringAsFixed(2)
+                        .replaceAll(".", ","),
                     color: CategoryType.alimentacao.color,
                   ),
                 ),
                 Expanded(
                   child: SummaryCard(
                     title: CategoryType.transporte.label,
-                    amount: "0,00",
+                    amount: totalCategoria(CategoryType.transporte)
+                        .toStringAsFixed(2)
+                        .replaceAll(".", ","),
                     color: CategoryType.transporte.color,
                   ),
                 ),
@@ -65,14 +125,18 @@ class _ControlePageState extends State<ControlePage> {
                 Expanded(
                   child: SummaryCard(
                     title: CategoryType.lazer.label,
-                    amount: "0,00",
+                    amount: totalCategoria(CategoryType.lazer)
+                        .toStringAsFixed(2)
+                        .replaceAll(".", ","),
                     color: CategoryType.lazer.color,
                   ),
                 ),
                 Expanded(
                   child: SummaryCard(
                     title: CategoryType.outros.label,
-                    amount: "0,00",
+                    amount: totalCategoria(CategoryType.outros)
+                        .toStringAsFixed(2)
+                        .replaceAll(".", ","),
                     color: CategoryType.outros.color,
                   ),
                 ),
@@ -86,6 +150,7 @@ class _ControlePageState extends State<ControlePage> {
                 Expanded(
                   flex: 2,
                   child: TextField(
+                    controller: descriptionController,
                     decoration: InputDecoration(
                       labelText: "Descrição",
                       border: OutlineInputBorder(),
@@ -96,6 +161,7 @@ class _ControlePageState extends State<ControlePage> {
                 Expanded(
                   flex: 1,
                   child: TextField(
+                    controller: amountController,
                     keyboardType: TextInputType.number,
                     decoration: InputDecoration(
                       labelText: "Valor",
@@ -133,7 +199,7 @@ class _ControlePageState extends State<ControlePage> {
                   },
                 ),
                 ElevatedButton.icon(
-                  onPressed: () {},
+                  onPressed: adicionarGasto,
                   icon: Icon(Icons.add),
                   label: Text("Adicionar"),
                   style: ElevatedButton.styleFrom(
@@ -149,14 +215,22 @@ class _ControlePageState extends State<ControlePage> {
 
             Expanded(
               child: ListView.builder(
-                itemCount: 3,
+                itemCount: min(expenses.length,7),
                 itemBuilder: (context, index) {
+                  final gasto = expenses[index];
+
                   return ExpenseTile(
-                    description: "almoço",
-                    amount: "35,00",
-                    categoryName: CategoryType.alimentacao.label,
-                    categoryColor: CategoryType.alimentacao.color,
-                    onDelete: () {},
+                    description: gasto.description,
+                    amount: gasto.amount
+                        .toStringAsFixed(2)
+                        .replaceAll(".", ","),
+                    categoryName: gasto.category.label,
+                    categoryColor: gasto.category.color,
+                    onDelete: () {
+                      setState(() {
+                        expenses.removeAt(index);
+                      });
+                    },
                   );
                 },
               ),
